c99c262913eee5c31a44ae3d802e2e78e65a6737
kcri <kcri@DESKTOP-4TTG0LD>
2020-11-17 13:46:18 +0900

    ignore 추가 재귀 - dp 문제 재풀이 필요

--- coding-test/src/common/Recursion_001.java
+++ coding-test/src/common/Recursion_001.java
@@ -1,17 +1,22 @@
 package common;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /* * 
- * 2020.11.14
+ * 2020.11.14/11.15/11.16/11.17
  * CodeUp (코드업)
  * https://codeup.kr/problemsetsol.php?psid=21
  * */
 public class Recursion_001 {
 
 	public void solution() {
-		question_1953(5,1);
+		question_4564();
+//		question_1953(5,1);
 	}
 	public void question_1901(int n, int cur) {
 		System.out.println(cur);
@@ -172,16 +177,111 @@ public class Recursion_001 {
 			question_1953(n-1, 1);
 		}
 	}
+	/* 실수로 재귀 아닌걸로 풀었는데, 삼각형 만드는 부분을 재귀로 하면 됨*/
 	public void question_3702() {
-		
+		int r = 3;
+		int c = 2;
+		int n = r>c?r:c;
+		int[][] triangle = getPascalTriangle(n);
+		System.out.println(triangle[r-1][c-1]);
 	}
+	public int[][] getPascalTriangle(int n) {
+		int[][] arr = new int[n][n];
+		for(int i=0; i <n ; i++) {
+			for(int j=0; j<n ;j++) {
+				//벽에 도달한 경우
+				if(i == 0 || j == 0) {
+					arr[i][j] = 1;
+				}
+				else {//벽이 아닌 경우
+					arr[i][j] = arr[i][j-1] + arr[i-1][j];
+				}
+			}
+		}
+		return arr;
+	}
+
+	/*2020.11.17 시간초과*/
+	private int stairsCnt = 0;
 	public void question_3704() {
-		
+		int answer = 0;
+		question_3704_1(3);
+		/* *
+		 * 1 or 2 or 3 계단씩 오를 수 있다.
+		 * 계단을 오를 수 있는 경우의 수 % 1000 
+		 * */
+		 answer = this.stairsCnt % 1000;
+		 System.out.println(answer);
+
 	}
+	public void question_3704_1(int n) {
+		if(n < 0) {
+			return;
+		}
+		if(n == 0) {
+			this.stairsCnt++;
+			return;
+		}
+		question_3704_1(n-1);
+		question_3704_1(n-2);
+		question_3704_1(n-3);
+	}
+	
+	/*시간초과*/
 	public void question_3733() {
+		int a=0;
+		int b =0;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		int max = 0;
+		int maxIdx = a;
+		for(int i=a;i<b+1;i++) {
+			int tempVal = question_3733_1(i,0);
+			if(tempVal > max) {
+				max = tempVal;
+				maxIdx = i;
+			}
+			tempVal = 0;
+		}
+		System.out.println( maxIdx +" " +max);
 		
 	}
+	public int question_3733_1(long n, int cnt) {
+		cnt++;
+		if(n == 1) {
+			return cnt;
+		}
+		if(n % 2 == 1) {
+			n = 3*n + 1;
+		}
+		else {
+			n = n/2;
+		}
+		return question_3733_1(n, cnt);
+	}
+	
+	/*대표적인 DP 문제 - 규칙성을 발견하여 수식을 만들어야함
+	 * = 백준 2579번
+	 * 설명 잘 되어있는 블로그
+	 * https://sundries-in-myidea.tistory.com/22
+	 * */
 	public void question_4564() {
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		try {
+			int T = Integer.parseInt(br.readLine());
+			int f[] = new int[T + 1];
+			int cache[] = new int[T + 1];
+			for (int i = 1; i <= T; i++) {
+				f[i] = Integer.parseInt(br.readLine());
+			}
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
 	}
 }
