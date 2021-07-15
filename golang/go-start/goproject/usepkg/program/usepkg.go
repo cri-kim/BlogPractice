325be3776acba5ad11bb8c0811e6cde73e48a664
DESKTOP-4TTG0LD\kcri <67crystalk@gmail.com>
2021-07-15 22:12:52 +0900

    example(go) : add chapter 16 about module & package

---
+++ golang/go-start/goproject/usepkg/program/usepkg.go
@@ -0,0 +1,17 @@
+package main
+
+import {
+	"fmt"
+	"goprojet/usepkg/custompkg"
+	//"github.com/guptarohit/asciigraph"
+	//"github.com/tuckersGo/musthaveGo/ch16/expkg"
+}
+
+func main(){
+	custompkg.PrintCustom()
+	//expkg.PrintCustom()
+
+	//data := []float64{3,4,5,6,7,8,9,6,4,4,33,1,2,3}
+	//graph := assciigraph.Plot(data)
+	//fmt.Println(graph)
+}
