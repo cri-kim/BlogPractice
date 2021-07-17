c9dd7e903f0a1e94d9d3b4a1b6664643e9ed8936
DESKTOP-4TTG0LD\kcri <67crystalk@gmail.com>
2021-07-17 18:54:46 +0900

    example(java) : 큐,스택,리스트 노드 구현 추가

--- java/data-structure/src/structure/ListQueue.java
+++ java/data-structure/src/structure/ListQueue.java
@@ -2,21 +2,41 @@ package structure;
 
 public class ListQueue implements Queue{
 
+	ListNode node;
+	ListNode head;
+	
 	@Override
 	public void offer(int item) {
-		// TODO Auto-generated method stub
-		
+		ListNode nodeToAdd = new ListNode(item);
+		if(isEmpty()) {
+			node = nodeToAdd;
+		}
+		else {
+			ListNode nextNode = node;
+			while(nextNode.getNext() != null) {
+				nextNode = nextNode.getNext();
+			}
+			nextNode.setNext(nodeToAdd);
+		}
 	}
 
 	@Override
 	public int poll() {
-		// TODO Auto-generated method stub
-		return 0;
+		if(isEmpty()) {
+			return -1;
+		}
+		int value = head.getItem();
+		ListNode nextNode = head.getNext();
+		head = null;
+		head = nextNode;
+		return value;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+		if(head.getNext() == null) {
+			return true;
+		}
 		return false;
 	}
 
