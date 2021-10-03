eae2d1a51d5e3334a6a1b5218ea158db6f270343
DESKTOP-4TTG0LD\kcri <67crystalk@naver.com>
2021-10-03 20:19:43 +0900

    refactor : gradle project로 변경

--- java/junit5/src/test/java/com/study/MyFirstJUnitJupiterTests.java
+++ java/junit5/src/test/java/com/study/MyFirstJUnitJupiterTests.java
@@ -1,4 +1,4 @@
-package basic;
+package com.study;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,41 +18,41 @@ import org.junit.jupiter.params.provider.ValueSource;
  * JUnit5 = JUnit Platform + JUnit Jupiter + JUnit Vintage
  * JUnit Jupiter = JUnit5에서 테스트를 위해 만들어진 프로그래밍 모델과 extension 모델으 조합이다.
  * 				 = TestEngine 제공
- * 
- * 
+ *
+ *
  * first setting
  * build path-> configurae build path -> libraries -> add Library -> select junit
  */
 public class MyFirstJUnitJupiterTests {
 
-	private final Caculator caculator = new Caculator();
-	
-	@BeforeAll
-	static void init() {
-		System.out.println("@BeforeAll - executes once before all test methods in this class");
-	}
-    
-	@BeforeEach
-	void setup() {
-		System.out.println("@BeforeEach - executes each before all test methods in this class");
-	}
-	
-	@Test
-	void addition() {
-		assertEquals(2, caculator.add(1, 1));
-	}
-	
-	class Caculator {
-		public int add(int a, int b) {
-			return a+b;
-		}
-	}
-	
-	@DisplayName("동작 성공")
-	@ParameterizedTest
-	@ValueSource(ints = {1,2,3,4,5,6,7,8,9,0})
-	void addtion2(int item) {
-		int result = item + 1;
-		assertEquals(result, caculator.add(item, 1));
-	}
+    private final Caculator caculator = new Caculator();
+
+    @BeforeAll
+    static void init() {
+        System.out.println("@BeforeAll - executes once before all test methods in this class");
+    }
+
+    @BeforeEach
+    void setup() {
+        System.out.println("@BeforeEach - executes each before all test methods in this class");
+    }
+
+    @Test
+    void addition() {
+        assertEquals(2, caculator.add(1, 1));
+    }
+
+    class Caculator {
+        public int add(int a, int b) {
+            return a+b;
+        }
+    }
+
+    @DisplayName("동작 성공")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5,6,7,8,9,0})
+    void addtion2(int item) {
+        int result = item + 1;
+        assertEquals(result, caculator.add(item, 1));
+    }
 }
