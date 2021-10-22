282cb57450f115f41f44fa87e5960752cdc3923b
kcri <67crystalk@naver.com>
2021-10-22 22:35:07 +0900

    feature : delete All 기능 추가

---
+++ spring/Ch2.Test/demo/src/main/java/com/example/demo/user/dao/UserDao.java
@@ -0,0 +1,54 @@
+package com.example.demo.user.dao;
+
+import com.example.demo.user.domain.User;
+import lombok.Data;
+
+import javax.sql.DataSource;
+import java.sql.*;
+
+@Data
+public class UserDao{
+
+    private DataSource dataSource;
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+                "insert into user(id, name, password) values(?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Connection c = dataSource.getConnection();
+        PreparedStatement ps = c.prepareStatement(
+                "select * from user where id = ?");
+        ps.setString(1, id);
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+        return user;
+    }
+
+    public void deleteAll() throws SQLException{
+        Connection c = dataSource.getConnection();
+        PreparedStatement ps = c.prepareStatement(
+                "delete from user");
+        ps.executeUpdate();
+        ps.close();
+        c.close();
+    }
+}
