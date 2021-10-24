fa5463ec8bced328f9ba48c83a61e79c59f39b05
kcri <67crystalk@naver.com>
2021-10-24 17:28:19 +0900

    refactor : template/callback 을 이용하여 리스트 더하기 기능 수정

--- spring/Ch3.Template/demo/src/test/java/learningtest/template/CalcSumTest.java
+++ spring/Ch3.Template/demo/src/test/java/learningtest/template/CalcSumTest.java
@@ -1,17 +1,23 @@
 package learningtest.template;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 public class CalcSumTest {
+    CalCultator cal;
+    String numFilePath;
+    @BeforeEach
+    void setup(){
+        cal = new CalCultator();
+        numFilePath = getClass()
+                .getResource("numbers.txt")
+                .getPath();
+    }
     @Test
     public void CalcSumTest() throws IOException {
-        CalCultator cal = new CalCultator();
-        int sum = cal.calcSum(getClass()
-                .getResource("numbers.txt")
-                .getPath());
-        assertThat(sum).isEqualTo(10);
+        assertThat(cal.calcSum(this.numFilePath)).isEqualTo(10);
     }
 }
