2b961fe4c144acfd0f7a8ae6b4ce18706bdac7f2
kcri <67crystalk@naver.com>
2021-11-06 17:08:33 +0900

    algorithm : failed leetcode15 too long

--- computer-science/java-algorithm-solution/src/main/java/array/LeetCode15.java
+++ computer-science/java-algorithm-solution/src/main/java/array/LeetCode15.java
@@ -2,20 +2,37 @@ package array;
 
 import java.util.*;
 /*
-* 모르겠다....(1hour do not solve)
+* 모르겠다....(1hour)
+* 2021-11-06 // 시간은 초과하지않았지만 too long  
 * */
 public class LeetCode15 {
+    public Set<List<Integer>> set = new HashSet<>();
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> rs = new ArrayList<>();
-        int n = nums.length;
-        if (n < 3) {
+        if(nums.length < 3){
             return new ArrayList<>();
         }
-        int x =0 ,y=0,z = 0;
-        //x = y + z
-        while(x < n){
 
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2 && nums[i] <= 0;i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            twoSum(nums, i);
         }
-        return rs;
+
+
+        return new ArrayList<>(set);
+    }
+
+    public void twoSum(int[] nums, int i){
+        for(int j=nums.length-1;j > i+1 ;j--){
+            if(j < nums.length-1 && nums[j] == nums[j+1]) continue;
+            int z = j-1;
+            while(z > i && nums[i] + nums[j] + nums[z] >= 0){
+                if(nums[i] + nums[j] + nums[z] == 0){
+                    set.add(Arrays.asList(nums[i],nums[j],nums[z]));
+                }
+                z--;
+            }
+        }
+
     }
 }
